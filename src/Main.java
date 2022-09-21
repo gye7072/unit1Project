3151eba9729762096aeedc45d5892e732bda7c6d
gye7072 <gye7072@bths.edu>
2022-09-21 17:07:26 -0400

    Fixed the Rounding Issue

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,29 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
         //set variables and ask for user input
-        System.out.println("Please input your bill: ");
+        System.out.println("---------------------");
+        System.out.print("Please input your bill: ");
         double bill = s.nextDouble();
-        System.out.println("Please input the tip percentage(whole number without percentage sign): ");
+        System.out.print("Please input the tip percentage(whole number without percentage sign): ");
         double tipPercent = s.nextDouble();
-        System.out.println("Please input the number of people in the party: ");
+        System.out.print("Please input the number of people in the party: ");
         int numPeople = s.nextInt();
+        System.out.println("---------------------");
 
         //calculate values using variables
-        double totalTip = Math.round(bill * (tipPercent * 1/100 * 100.0))/100.0;
-        double totalBill = Math.round(bill + totalTip);
-        double tipPerPerson = Math.round(totalTip/numPeople * 10.0)/10.0;
-        double totalPerPerson = Math.round(totalBill/numPeople * 10.0)/10.0;
+        double totalTip = Math.round(bill * (tipPercent/100.0 * 100.0))/100.0;
+        double totalBill = Math.round(( bill + totalTip)*100.0)/100.0;
+        double tipPerPerson = Math.round(totalTip/numPeople * 100.0)/100.0;
+        double totalPerPerson = Math.round(totalBill/numPeople * 100.0)/100.0;
 
         //display values
+        System.out.println();
+        System.out.println("---------------------");
         System.out.println("Total Tip: $ " + totalTip);
         System.out.println("Total Bill: $ " + totalBill);
         System.out.println("Total Tip Per Person : $ " + tipPerPerson);
         System.out.println("Total Total Per Person: $ " + totalPerPerson);
-
+        System.out.println("---------------------");
 
     }
 }
